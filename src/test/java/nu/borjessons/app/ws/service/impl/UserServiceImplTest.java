6195736f2ed68ce085c1c0a96eaa1fc07b1db272
Rinbo <robin.b@outlook.com>
2019-05-06 20:40:27 +0200

    Mock userRepository and stub out a userEntity object

--- src/test/java/nu/borjessons/app/ws/service/impl/UserServiceImplTest.java
+++ src/test/java/nu/borjessons/app/ws/service/impl/UserServiceImplTest.java
@@ -4,16 +4,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import nu.borjessons.app.ws.io.entity.UserEntity;
+import nu.borjessons.app.ws.io.repositories.UserRepository;
 
 class UserServiceImplTest {
+	
+	@InjectMocks
+	UserServiceImpl userService;
+
+	@Mock
+	UserRepository userRepository;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
 	}
 
 	@Test
 	void testGetUser() {
-		fail("Not yet implemented");
+		UserEntity userEntity = new UserEntity();
+		userEntity.setId(1L);
+		userEntity.setUserId("1323kjlkedf");
+		userEntity.setFirstName("Robin");
+		userEntity.setLastName("Börjesson");
+		when(userRepository.findByEmail(anyString())).thenReturn(userEntity);
 	}
 
 }
