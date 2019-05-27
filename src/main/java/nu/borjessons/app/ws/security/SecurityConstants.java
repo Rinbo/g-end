5d2aff05481aee0eb19eec8b19d455b6758b2225
Rinbo <robin.b@outlook.com>
2019-05-27 20:12:25 +0200

    Add antmatchers permit all for post and get for graphs/**

--- src/main/java/nu/borjessons/app/ws/security/SecurityConstants.java
+++ src/main/java/nu/borjessons/app/ws/security/SecurityConstants.java
@@ -7,10 +7,10 @@ public class SecurityConstants {
 	public static final long PASSWORD_RESET_EXPIRATION_TIME = 3600000;
 	public static final String TOKEN_PREFIX = "Bearer";
 	public static final String HEADER_STRING = "Authorization";
-	public static final String SIGN_UP_URL = "/users";
-	//public static final String TOKEN_SECRET = "kdlfojadf84hjjf3ddffe34v";
+	public static final String SIGN_UP_URL = "/users";	
 	public static final String VERIFICATION_EMAIL_URL = "/users/email-verification";
 	public static final String PASSWORD_RESET_REQUEST_URL = "/users/password-reset-request";
+	public static final String PASSWORD_RESET_URL = "/users/password-reset";
 
 	public static String getTokenSecret() {
 		AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
